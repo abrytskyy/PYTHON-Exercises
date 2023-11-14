31ebb25e3a4a99c7f296f7a6e7be5e0081a55d8e
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2023-11-14 08:40:00 +0100

    Add files via upload

---
+++ JEVGEN/08.15.py
@@ -0,0 +1,12 @@
+a = int(input("Input a: "))
+b = int(input("Input b: "))
+
+result = 3 * (a + b)**3 + 275 * b**2 - 127 * a - 41
+print(result)
+
+separator = input("Введіть рядок-розділювач: ")
+print("Money often" + separator + "costs" + separator + "too much")
+
+
+
+
